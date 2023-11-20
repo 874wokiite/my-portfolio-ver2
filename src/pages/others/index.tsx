866fe65c4ee6d01d60e999c42a008ac35ad7d289
874wokiite <f.kanonon@gmail.com>
2023-11-21 03:07:56 +0900

    :lipstick: Add pages

---
+++ src/pages/others/index.tsx
@@ -0,0 +1,9 @@
+const Others = () => {
+  return (
+    <div>
+        <p>This is others page!</p>
+    </div>
+  )
+}
+
+export default Others
---
+++ src/pages/projects/index.tsx
@@ -0,0 +1,9 @@
+const Projects = () => {
+  return (
+    <div>
+        <p>This is projects page!</p>
+    </div>
+  )
+}
+
+export default Projects
